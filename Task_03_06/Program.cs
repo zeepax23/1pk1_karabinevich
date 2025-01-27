436175ba36bffe2910f50e860d8e7bc1bb1aa213
zeepax23 <kkuuwll.23@gmail.com>
2025-01-27 10:35:21 +0500

    выполнена задача 06 из урока 03

---
+++ Task_03_06/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Task_03_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Написать программу, которая выводит таблицу значений функции: 𝑦=|𝑥|для -4 ≤ x ≤ 4, с шагом h = 0,5.
+
+            double startX = -4.0;
+            double endX = 4.0;
+            double step = 0.5;
+            for (double x = startX; x <= endX; x += step)
+            {
+                double y = Math.Abs(x);
+                Console.WriteLine($"{x,6:F2} | {y,8:F2}");
+
+
+            }
+        }
+    }
+}
