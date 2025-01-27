d5abcb17b2873c79ac17baa37ef1794937a807d2
zeepax23 <kkuuwll.23@gmail.com>
2025-01-27 10:13:34 +0500

    выполнена 02 задача урока 02

---
+++ Task_02_02/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Task_02_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ввод данных
+            Console.WriteLine("введите первое число (a)");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("введите второе число (b)");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            //вычисление и вывод результата
+            double summAB = a + b;
+            Console.WriteLine("сумма чисел: " + summAB);
+
+            double multipleAB = a * b;
+            Console.WriteLine("произведение чисел: " + multipleAB);
+            double diffAB = a - b;
+            Console.WriteLine("разность чисел: " + diffAB);
+            double divAB;
+            if (b != 0)
+            {
+                divAB = a / b;
+                Console.WriteLine("деление a на b: " + divAB);
+            }
+            else
+            {
+                Console.WriteLine("деление на ноль");
+            }
+        }
+    }
+}
