db762a8e90c29e7e53878d2e91e6cffb74f3e941
zeepax23 <kkuuwll.23@gmail.com>
2025-01-27 10:29:47 +0500

    выполнено 04 задача из урока 03

---
+++ Task_03_04/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Task_03_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter texts: ");
+            while (true)
+            {
+                string user = Console.ReadLine();
+                if (user == "exit")
+                {
+
+                    break;
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Enter texts: ");
+
+
+                    continue;
+                }
+            }
+        }
+    }
+}
